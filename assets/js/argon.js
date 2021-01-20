d357a901c66a1b5fcda036973ad6acf383698069
pratheesh <pratheesh@techversant>
2021-01-20 14:45:27 +0530

    Initial commit

---
+++ assets/js/argon.js
@@ -0,0 +1,424 @@
+
+/*!
+
+=========================================================
+* Argon Dashboard - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md)
+
+* Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+
+
+//
+// Layout
+//
+
+'use strict';
+
+var Layout = (function() {
+
+    function pinSidenav() {
+        $('.sidenav-toggler').addClass('active');
+        $('.sidenav-toggler').data('action', 'sidenav-unpin');
+        $('body').removeClass('g-sidenav-hidden').addClass('g-sidenav-show g-sidenav-pinned');
+        $('body').append('<div class="backdrop d-xl-none" data-action="sidenav-unpin" data-target='+$('#sidenav-main').data('target')+' />');
+
+        // Store the sidenav state in a cookie session
+        Cookies.set('sidenav-state', 'pinned');
+    }
+
+    function unpinSidenav() {
+        $('.sidenav-toggler').removeClass('active');
+        $('.sidenav-toggler').data('action', 'sidenav-pin');
+        $('body').removeClass('g-sidenav-pinned').addClass('g-sidenav-hidden');
+        $('body').find('.backdrop').remove();
+
+        // Store the sidenav state in a cookie session
+        Cookies.set('sidenav-state', 'unpinned');
+    }
+
+    // Set sidenav state from cookie
+
+    var $sidenavState = Cookies.get('sidenav-state') ? Cookies.get('sidenav-state') : 'pinned';
+
+    if($(window).width() > 1200) {
+        if($sidenavState == 'pinned') {
+            pinSidenav()
+        }
+
+        if(Cookies.get('sidenav-state') == 'unpinned') {
+            unpinSidenav()
+        }
+
+        $(window).resize(function() {
+            if( $('body').hasClass('g-sidenav-show') && !$('body').hasClass('g-sidenav-pinned')) {
+                $('body').removeClass('g-sidenav-show').addClass('g-sidenav-hidden');
+            }
+        })
+    }
+
+    if($(window).width() < 1200){
+      $('body').removeClass('g-sidenav-hide').addClass('g-sidenav-hidden');
+      $('body').removeClass('g-sidenav-show');
+      $(window).resize(function() {
+          if( $('body').hasClass('g-sidenav-show') && !$('body').hasClass('g-sidenav-pinned')) {
+              $('body').removeClass('g-sidenav-show').addClass('g-sidenav-hidden');
+          }
+      })
+    }
+
+
+
+    $("body").on("click", "[data-action]", function(e) {
+
+        e.preventDefault();
+
+        var $this = $(this);
+        var action = $this.data('action');
+        var target = $this.data('target');
+
+
+        // Manage actions
+
+        switch (action) {
+            case 'sidenav-pin':
+                pinSidenav();
+            break;
+
+            case 'sidenav-unpin':
+                unpinSidenav();
+            break;
+
+            case 'search-show':
+                target = $this.data('target');
+                $('body').removeClass('g-navbar-search-show').addClass('g-navbar-search-showing');
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-showing').addClass('g-navbar-search-show');
+                }, 150);
+
+                setTimeout(function() {
+                    $('body').addClass('g-navbar-search-shown');
+                }, 300)
+            break;
+
+            case 'search-close':
+                target = $this.data('target');
+                $('body').removeClass('g-navbar-search-shown');
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-show').addClass('g-navbar-search-hiding');
+                }, 150);
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-hiding').addClass('g-navbar-search-hidden');
+                }, 300);
+
+                setTimeout(function() {
+                    $('body').removeClass('g-navbar-search-hidden');
+                }, 500);
+            break;
+        }
+    })
+
+
+    // Add sidenav modifier classes on mouse events
+
+    $('.sidenav').on('mouseenter', function() {
+        if(! $('body').hasClass('g-sidenav-pinned')) {
+            $('body').removeClass('g-sidenav-hide').removeClass('g-sidenav-hidden').addClass('g-sidenav-show');
+        }
+    })
+
+    $('.sidenav').on('mouseleave', function() {
+        if(! $('body').hasClass('g-sidenav-pinned')) {
+            $('body').removeClass('g-sidenav-show').addClass('g-sidenav-hide');
+
+            setTimeout(function() {
+                $('body').removeClass('g-sidenav-hide').addClass('g-sidenav-hidden');
+            }, 300);
+        }
+    })
+
+
+    // Make the body full screen size if it has not enough content inside
+    $(window).on('load resize', function() {
+        if($('body').height() < 800) {
+            $('body').css('min-height', '100vh');
+            $('#footer-main').addClass('footer-auto-bottom')
+        }
+    })
+
+})();
+
+
+//
+// Popover
+//
+
+'use strict';
+
+var Popover = (function() {
+
+	// Variables
+
+	var $popover = $('[data-toggle="popover"]'),
+		$popoverClass = '';
+
+
+	// Methods
+
+	function init($this) {
+		if ($this.data('color')) {
+			$popoverClass = 'popover-' + $this.data('color');
+		}
+
+		var options = {
+			trigger: 'focus',
+			template: '<div class="popover ' + $popoverClass + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+		};
+
+		$this.popover(options);
+	}
+
+
+	// Events
+
+	if ($popover.length) {
+		$popover.each(function() {
+			init($(this));
+		});
+	}
+
+})();
+
+//
+// Tooltip
+//
+
+'use strict';
+
+var Tooltip = (function() {
+
+	// Variables
+
+	var $tooltip = $('[data-toggle="tooltip"]');
+
+
+	// Methods
+
+	function init() {
+		$tooltip.tooltip();
+	}
+
+
+	// Events
+
+	if ($tooltip.length) {
+		init();
+	}
+
+})();
+
+//
+// Form control
+//
+
+'use strict';
+
+var FormControl = (function() {
+
+	// Variables
+
+	var $input = $('.form-control');
+
+
+	// Methods
+
+	function init($this) {
+		$this.on('focus blur', function(e) {
+        $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus'));
+    }).trigger('blur');
+	}
+
+
+	// Events
+
+	if ($input.length) {
+		init($input);
+	}
+
+})();
+
+//
+// Bootstrap Datepicker
+//
+
+'use strict';
+
+var Datepicker = (function() {
+
+	// Variables
+
+	var $datepicker = $('.datepicker');
+
+
+	// Methods
+
+	function init($this) {
+		var options = {
+			disableTouchKeyboard: true,
+			autoclose: false
+		};
+
+		$this.datepicker(options);
+	}
+
+
+	// Events
+
+	if ($datepicker.length) {
+		$datepicker.each(function() {
+			init($(this));
+		});
+	}
+
+})();
+
+
+//
+// Bootstrap DateTimepicker
+//
+
+'use strict';
+
+var Datepicker = (function() {
+
+	// Variables
+
+	var $datepicker = $('.datetimepicker');
+
+
+	// Methods
+
+	function init($this) {
+		var options = {
+			// disableTouchKeyboard: true,
+			// autoclose: false
+			icons : {
+				down :"fa fa-arrow-down",
+				up :"fa fa-arrow-up",
+				time : "fa fa-clock",
+			},
+			// closeOnWithoutClick :false,
+
+		};
+
+		$this.datetimepicker(options);
+	}
+
+
+	// Events
+
+	if ($datepicker.length) {
+		$datepicker.each(function() {
+			init($(this));
+		});
+	}
+
+})();
+
+//
+// Form control
+//
+
+'use strict';
+
+var noUiSlider = (function() {
+
+	// Variables
+
+	// var $sliderContainer = $('.input-slider-container'),
+	// 		$slider = $('.input-slider'),
+	// 		$sliderId = $slider.attr('id'),
+	// 		$sliderMinValue = $slider.data('range-value-min');
+	// 		$sliderMaxValue = $slider.data('range-value-max');;
+
+
+	// // Methods
+	//
+	// function init($this) {
+	// 	$this.on('focus blur', function(e) {
+  //       $this.parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+  //   }).trigger('blur');
+	// }
+	//
+	//
+	// // Events
+	//
+	// if ($input.length) {
+	// 	init($input);
+	// }
+
+
+
+	if ($(".input-slider-container")[0]) {
+			$('.input-slider-container').each(function() {
+
+					var slider = $(this).find('.input-slider');
+					var sliderId = slider.attr('id');
+					var minValue = slider.data('range-value-min');
+					var maxValue = slider.data('range-value-max');
+
+					var sliderValue = $(this).find('.range-slider-value');
+					var sliderValueId = sliderValue.attr('id');
+					var startValue = sliderValue.data('range-value-low');
+
+					var c = document.getElementById(sliderId),
+							d = document.getElementById(sliderValueId);
+
+					noUiSlider.create(c, {
+							start: [parseInt(startValue)],
+							connect: [true, false],
+							//step: 1000,
+							range: {
+									'min': [parseInt(minValue)],
+									'max': [parseInt(maxValue)]
+							}
+					});
+
+					c.noUiSlider.on('update', function(a, b) {
+							d.textContent = a[b];
+					});
+			})
+	}
+
+	if ($("#input-slider-range")[0]) {
+			var c = document.getElementById("input-slider-range"),
+					d = document.getElementById("input-slider-range-value-low"),
+					e = document.getElementById("input-slider-range-value-high"),
+					f = [d, e];
+
+			noUiSlider.create(c, {
+					start: [parseInt(d.getAttribute('data-range-value-low')), parseInt(e.getAttribute('data-range-value-high'))],
+					connect: !0,
+					range: {
+							min: parseInt(c.getAttribute('data-range-value-min')),
+							max: parseInt(c.getAttribute('data-range-value-max'))
+					}
+			}), c.noUiSlider.on("update", function(a, b) {
+					f[b].textContent = a[b]
+			})
+	}
+
+})();
