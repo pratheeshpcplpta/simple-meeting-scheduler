6bcdd2307322ea3218d5e0d5c0ec20341806fe16
pratheesh <pratheesh@techversant>
2021-01-21 18:54:26 +0530

    updates

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # simple-meeting-scheduler
 
-## Installation
+## How to run
+- go to path server
+- execute binary server
+    ```
+    ./server --runserver
+    ```
+- Open http://localhost:9091/ in browser
+
+- users
+    ```
+
+    username    password
+    --------------------
+    username    username
+    user1       user1
+    user2       user2
+    user3       user3
+    ```
---
+++ app/assets/js/api.js
@@ -0,0 +1,261 @@
+$(document).ready(function(){
+    $(".error-display").hide()
+
+    let APIName = "api_thirdparty"
+    let APIKey = "AAhzclm9jHdypqdmEQx"
+    // 
+    // Redirect if cookie seted
+    // 
+    token = Cookies.get("access_token")
+    if (token != "" && window.location.pathname == "/" ) {
+        window.location = "/dashboard"
+    }
+
+    if (token == "" && window.location.pathname != "/" ) {
+        window.location = "/"
+    }
+    
+
+
+    var apiEndpoint = "http://localhost:9091/api"
+
+    $("#login_button").on("click", function(){
+        let username = $(".field-username").val()
+        let password = $(".field-password").val()
+
+        var form = new FormData();
+        form.append("username", username);
+        form.append("password", password);
+
+        var settings = {
+            "url": apiEndpoint + "/login",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "X-API-KEY": APIKey,
+                "X-API-NAME": APIName,
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            data = JSON.parse(response)
+            if (data.status == "success") {
+                Cookies.set('access_token', data.data["token"]);
+                window.location = "/dashboard"
+            }
+
+            if (data.status == "error") {
+                $(".error-display").html(data.message)
+                $(".error-display").show()
+            } 
+        });
+    })
+
+    // 
+    // Upcoming Meetings
+    // 
+    if ($(".upcoming-meetings").length > 0 ){
+        var form = new FormData();
+
+        var settings = {
+            "url": apiEndpoint + "/list-meetings/upcoming",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "X-API-KEY": APIKey,
+                "X-API-NAME": APIName,
+                "X-ACCESS-TOKEN" : token,
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            data = JSON.parse(response)
+            if (data.status == "success") {
+                let html_tag = template__meetings(data.data)
+                $(".upcoming-meetings .upcoming-meetings--table").html(html_tag)
+            }
+
+            if (data.status == "error") {
+                $(".error-display").html(data.message)
+                $(".error-display").show()
+            } 
+        });
+    } 
+
+     // 
+    // Upcoming Meetings
+    // 
+    if ($(".recent-meetings").length > 0 ){
+        var form = new FormData();
+
+        var settings = {
+            "url": apiEndpoint + "/list-meetings/recent",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "X-API-KEY": APIKey,
+                "X-API-NAME": APIName,
+                "X-ACCESS-TOKEN" : token,
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            data = JSON.parse(response)
+            if (data.status == "success") {
+                let html_tag = template__meetings(data.data)
+                $(".recent-meetings .recent-meetings--table").html(html_tag)
+            }
+
+            if (data.status == "error") {
+                $(".error-display").html(data.message)
+                $(".error-display").show()
+            } 
+        });
+    } 
+    
+
+    function template__meetings( data ) {
+        let html = ""
+        $.each(data, function(index,val) {
+            meeting__id = (val.meeting_id != null) ? val.meeting_id : "-"
+            html += '<tr> \
+                        <th scope="row">\
+                        <div class="media align-items-center">\
+                            <div class="media-body">\
+                            <span class="name mb-0 text-sm">'+meeting__id+'</span>\
+                            </div>\
+                        </div>\
+                        </th>\
+                        <td>\
+                        '+val.title+'\
+                        </td>\
+                        <td>\
+                        <div class="avatar-group">\
+                            <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Ryan Tompson">\
+                            <img alt="Image placeholder" src="assets/img/user.png">\
+                            </a>\
+                            <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Romina Hadid">\
+                            <img alt="Image placeholder" src="assets/img/user.png">\
+                            </a>\
+                            <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Alexander Smith">\
+                            <img alt="Image placeholder" src="assets/img/user.png">\
+                            </a>\
+                            <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Jessica Doe">\
+                            <img alt="Image placeholder" src="assets/img/user.png">\
+                            </a>\
+                        </div>\
+                        </td>\
+                        <td>\
+                        '+new Date(val.start_time*1000).toLocaleString()+' - '+new Date((val.end_time*1000)).toLocaleString()+'\
+                        </td>\
+                    </tr>'
+        })
+
+        return html
+    }
+
+
+
+
+    // 
+    // List users
+    // 
+    if ($(".field-schedule--participants").length > 0) {
+        var form = new FormData();
+        var settings = {
+            "url": apiEndpoint + "/get-users",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "X-API-KEY": APIKey,
+                "X-API-NAME": APIName,
+                "X-ACCESS-TOKEN" : token,
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            data = JSON.parse(response)
+            if (data.status == "success") {
+                let html__option = ""
+                $.each(data.data, function(key,val){
+                    html__option += '<option value="'+val.id+'">'+val.username+'</option>'
+                });
+
+                $(".field-schedule--participants").html(html__option)
+            }
+
+            if (data.status == "error") {
+                $(".error-display").html(data.message)
+                $(".error-display").show()
+            } 
+        });
+    }
+
+    // 
+    // On schedule 
+    // 
+    $(".schedule-meeting-submit").on("click", function(){
+        var form = new FormData();
+        form.append("Title", $(".field-schedule--title").val());
+        form.append("Description", $(".field-schedule--description").val());
+        form.append("StartTime", $(".field-schedule--starttime").val());
+        form.append("EndTime", $(".field-schedule--endtime").val());
+        form.append("Participants", $(".field-schedule--participants").val());
+
+        startTime = new Date($(".field-schedule--starttime").val())
+        endTime = new Date($(".field-schedule--starttime").val())
+
+        if(startTime >= endTime){
+            $(".error-display").html("Start tim should be less than end time")
+            $(".error-display").show()
+            return
+        }
+        var settings = {
+            "url": apiEndpoint + "/schedule-meeting",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "X-API-KEY": APIKey,
+                "X-API-NAME": APIName,
+                "X-ACCESS-TOKEN" : token,
+            },
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+
+        $.ajax(settings).done(function (response) {
+            data = JSON.parse(response)
+            if (data.status == "success") {
+                window.location = "/dashboard"
+            }
+
+            if (data.status == "error") {
+                $(".error-display").html(data.message)
+                $(".error-display").show()
+            } 
+        });
+    })
+
+    $(".logout").on("click", function(){
+        Cookies.set("access_token","")
+        window.location = '/'
+    })
+})
